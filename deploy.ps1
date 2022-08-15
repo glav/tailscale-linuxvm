c4a7a03403126ad2807b68083af3d9ed39f64371
Paul Glavich <glav@theglavs.com>
2022-08-15 18:04:34 +1000

    Added in expiresOn tag

--- deploy.ps1
+++ deploy.ps1
@@ -8,6 +8,9 @@ param (
 
 $rg="tailscale-test"
 
-az group create --location AustraliaEast --resource-group $rg
+#Ensure the subscription cleaner deletes this group after  one day
+#TODO: Parametrise this so that we can optionally pass in the expiresOn tag value or not.
+$deleteDate = get-date -Format yyyy-MM-dd
+az group create --location AustraliaEast --resource-group $rg --tags expiresOn=$deleteDate
 
 az deployment group create --resource-group $rg --template-file .\main.bicep  --parameters vmAdminUsername=$AdminUserName vmAdminPassword=$AdminUserPassword
